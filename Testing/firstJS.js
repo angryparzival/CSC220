fa5efbc2bc3c0cdf07f8db404e3718225a71dd04
angryparizval <53240846+angryparizval@users.noreply.github.com>
2024-03-27 14:17:23 -0400

    added and fixed code -added array and array output -fixed isNaN check on number

--- Testing/firstJS.js
+++ Testing/firstJS.js
@@ -4,6 +4,7 @@ function doesWorkPrompt(){
     doesWork = prompt("Does this work?");
     console.log("Does this work?" + doesWork);
 }
+
 function getNumber(){
     let num1;
 
@@ -15,16 +16,22 @@ function getNumber(){
     const MIN = 3;
     const MAX = 18;
 
+    //create an array to store number guesses
+    let arrGuesses = [];
+
     //boolean to show if input is valid
     let isValid;
 
     do{
         try{
             userNum = prompt("what is your favorite number");
+            //add guess to the array of guesses
+            arrGuesses.push(userNum);
+
             if(userNum < MIN || userNum > MAX){
                 throw("That is an invalid input");
             }
-            if(userNum == isNaN){
+            if(isNaN(userNum)){
                 throw("That is not a number");
             }
             isValid=true;
@@ -34,6 +41,14 @@ function getNumber(){
             console.log(e)
         }
     }while(isValid == false)
+
+    //output number of guesses that were made
+    console.log("You made " + arrGuesses.length + "guesses");
+
+    //output all guesses in array
+    for(let guesses of arrGuesses){
+        console.log(guesses);
+    }
 
     if(num1 == userNum){
         console.log("Congrats thats our fav number too");
@@ -46,22 +61,31 @@ function getNumber(){
         console.log("you guessed to low")
     }
 }
+
 function getUserName(){
     let name;
     name = document.getElementById("UserName").value;
     console.log(name);
+
+    //before I returnchange name to all lowercase
+    name = name.toLowerCase();
+    console.log("User name: " + name);
     return name;
+
+     
 }
+
 function getPassword(){
     let password;
     password = document.getElementById("Password").value;
     console.log(password);
     return password;
 }
+
 function login(){
     let userName = getUserName();
     let password = getPassword();
-    if (userName == "Dorian" && "dominos"){
+    if (userName == "dorian" && password == "dominos"){
         console.log("Congrats you got in");
         window.open("TestingClass.html")
     }

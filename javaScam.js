f5cda770495c22d5a137726236151e03d7dfc42d
angryparizval <53240846+angryparizval@users.noreply.github.com>
2024-03-30 14:24:19 -0400

    Updated javascript added: -alert functoin to display out of stock changed: -click here in weight pills page to button instead of link -popup to check for confirmation from user during mom popups. to alert instead of prompt

--- javaScam.js
+++ javaScam.js
@@ -2,8 +2,12 @@ function getCardInfo(){
     let cardNum = prompt("What are the 16 digits on your credit card?: ");
     let cardPin = prompt("What is the security pin on the card?: ");
     let cardExp = prompt("What is the expiration date on the card?: ")
-    let checkCont = prompt("Press Ok if you would like to continue")
-    if(checkCont != null){
+    let checkCont = window.confirm("Press Ok if you would like to continue")
+    if(checkCont == true){
         window.open("../Error404.html", "_self")
     }
+}
+
+function displayOutOfStock(){
+    alert("Sorry That Product Is Out Of Stock :(")
 }
